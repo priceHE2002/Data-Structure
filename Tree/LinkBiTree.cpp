2d97d87f8649d491d71fd5a03fc92572811bc74a
何宇航 <heyuhang@heyuhangdeMacBook-Air.local>
2026-04-08 16:31:13 +0800

    新增层序遍历

--- Tree/LinkBiTree.cpp
+++ Tree/LinkBiTree.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 typedef struct BiTNode{
     int value;
@@ -130,6 +132,124 @@ void PostOrder(BiTree T)
     }
 }
 
+//求树的深度
+int treeDepth(BiTree T)
+{
+    if(T == nullptr)
+    {
+        return 0;
+    }
+    //树的深度 = max（左子树，右子树）+ 1
+    else {
+        int l = treeDepth(T->leftChild);
+        int r = treeDepth(T->rightChild);
+        return r > l ? r + 1 : l + 1;
+    }
+}
+
+//辅助队列结点
+struct LinkNode
+{
+    BiTNode *data;
+    LinkNode *next;
+};
+
+//辅助队列
+struct LinkQueue
+{
+    LinkNode *front, *rear;
+};
+
+//辅助队列的初始化
+bool InitLinkQueue(LinkQueue &Q)
+{
+    LinkNode *node = new LinkNode;
+    if(node == nullptr)
+    {
+        return false;
+    }
+    node->next = nullptr;
+    Q.front = Q.rear = node;
+    return true;
+}
+
+//辅助队列判空
+bool IsLinkQueueEmpty(LinkQueue Q)
+{
+    return Q.front == Q.rear;
+}
+
+//入队
+bool enLinkQueue(LinkQueue &Q, BiTNode *e)
+{
+    LinkNode *newNode = new LinkNode;
+    if(newNode == nullptr)
+    {
+        cout<<"新结点内存分配失败，入队失败"<<endl;
+        return false;
+    }
+    newNode->data =e;
+    newNode->next = nullptr;
+    Q.rear->next = newNode;
+    Q.rear = Q.rear->next;
+    return true;
+}
+
+//出队，出队
+bool deLinkQueue(LinkQueue &Q, BiTNode *&e)
+{
+    if(Q.front == Q.rear)
+    {
+        cout<<"空列为空，出队失败"<<endl;
+        return false;
+    }
+    LinkNode *p = Q.front->next;
+    e = p->data;
+    Q.front->next = p->next;
+    if(Q.rear == p)
+    {
+        Q.rear = Q.front;
+    }
+    delete p;
+    return true;
+}
+
+bool DestroyLinkQueue(LinkQueue &Q)
+{
+    LinkNode *p = Q.front;  //从队头开始
+    while(p != nullptr)     //遍历所有结点
+    {
+        Q.front = p->next;  //先保存下一个结点
+        delete p;           //删除当前结点
+        p = Q.front;        //移动到下一个结点
+    }
+    Q.front = Q.rear = nullptr;  //清空队列指针
+    return true;
+}
+
+//层序遍历
+void levelOrder(BiTree T)
+{
+    LinkQueue Q;
+    InitLinkQueue(Q);
+    BiTree p;
+    enLinkQueue(Q, T);
+    while(!IsLinkQueueEmpty(Q))
+    {
+        deLinkQueue(Q, p);
+        visit(p);
+        if(p->leftChild != nullptr)
+        {
+            enLinkQueue(Q, p->leftChild);
+        }
+        if(p->rightChild != nullptr)
+        {
+            enLinkQueue(Q, p->rightChild);
+        }
+    }
+    DestroyLinkQueue(Q);
+}
+
 int main()
 {
     BiTree root;
@@ -155,6 +275,10 @@ int main()
     // 后序遍历
     cout << "后序遍历: ";
     PostOrder(root);
+    cout << endl;
+
+    cout << "层序遍历: ";
+    levelOrder(root);
     cout << endl;
 
     return 0;
